5c69b66e4d2d405b254518fe1eb11be62cbf0ab2
Angel Buenrostro <abuenrostro@boomtownroi.com>
2020-07-26 20:07:08 -0400

    Blank canvas

---
+++ ArtCanvas/Shared/ArtCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCanvasApp.swift
+//  Shared
+//
+//  Created by Angel Buenrostro on 7/26/20.
+//
+
+import SwiftUI
+
+@main
+struct ArtCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
